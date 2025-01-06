46d95fa48463b683ff0ab7e3a0a3e90e34d37573
Francisco Damião Terto <contato@damiaoterto.dev>
2025-01-06 13:26:53 -0300

    feat: implement app execution

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/damiaoterto/ip-finder/app"
+)
+
 func main() {
-	// TODO
+	app := app.Generate()
+	if err := app.Run(os.Args); err != nil {
+		log.Fatalln(err)
+	}
 }
